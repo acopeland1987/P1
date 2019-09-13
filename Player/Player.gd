9ee51aba3b824ffb965d3e2e6b1583538e3207c6
Adrian <acopeland.ca@gmail.com>
2019-09-12 23:37:23 -0400

    edit

--- Player/Player.gd
+++ Player/Player.gd
@@ -13,21 +13,22 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
-		$anim.play("left")
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
-		$anim.play("right")
 	else:
 		velocity.x = 0
-	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
-		velocity.y = -JUMP_POWER
 		
+	if Input.is_action_pressed("ui_jump") and is_on_floor():
+		velocity.y = -JUMP_POWER
+		$anim.play("jumpright")
 	if Input.is_action_just_pressed("ui_dash"):
 		dash()
 		
 	velocity.y += GRAVITY
 	velocity = move_and_slide(velocity, FLOOR)
-
+		
+		
+	
 func dash():
 	SPEED *= DASH_POWER
 	dashing = true
