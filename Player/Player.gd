4b9961abeb9c72b2ca0eb501344fd4e74576b41e
Adrian <acopeland.ca@gmail.com>
2019-09-12 22:30:54 -0400

    edit

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,9 +2,11 @@ extends KinematicBody2D
 
 var SPEED = 190
 var GRAVITY = 15
-var JUMP_POWER = 350
+var JUMP_POWER = 320
+var DASH_POWER = 5
 var FLOOR = Vector2(0,-1)
 
+var dashing = false
 var velocity = Vector2()
 
 func _physics_process(delta):
@@ -20,6 +22,18 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
 		velocity.y = -JUMP_POWER
 		
-	velocity.y += GRAVITY
+	if Input.is_action_just_pressed("ui_dash"):
+		dash()
 		
+	velocity.y += GRAVITY
 	velocity = move_and_slide(velocity, FLOOR)
+
+func dash():
+	SPEED *= DASH_POWER
+	dashing = true
+	$Timer.start()
+
+
+func _on_Timer_timeout():
+	SPEED = 190
+	dashing = false
